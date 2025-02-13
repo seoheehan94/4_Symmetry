e098201beb2af962279848f99c6d7754899582cd
seoheehan94 <seohee.han@mail.utoronto.ca>
2025-02-13 15:12:58 -0500

    Update runAllFiles2.m

--- runAllFiles2.m
+++ runAllFiles2.m
@@ -2,26 +2,21 @@
 methods = {'contour', 'medialAxis', 'area'};
 
 % addpath(genpath('/home/hanseohe/Documents/GitHub/3rdParty'))
-  curmethod = 3;
-    for sub = 1:8
-        for roi = 2:4
-            fprintf('%s. %d. %d. %s ...\n','prfSampleModel',sub,roi, methods{curmethod});
-            prfSampleModel_symmetry(sub,roi, methods{curmethod});
-        end
-    end
+  % curmethod = 3;
+  %   for sub = 1:8
+  %       for roi = 2:4
+  %           fprintf('%s. %d. %d. %s ...\n','prfSampleModel',sub,roi, methods{curmethod});
+  %           prfSampleModel_symmetry(sub,roi, methods{curmethod});
+  %       end
+  %   end
 
 %% regressPrfSplit
-% for sub = 1:8
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
-% regressPrfSplit(sub, 1);
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 2);
-% regressPrfSplit(sub, 2);
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 3);
-% regressPrfSplit(sub, 3);
-% fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 4);
-% regressPrfSplit(sub, 4);
-% end
-% % 
+for sub = 1:8
+fprintf('%s. %d. %d. %s ...\n','regressPrfSplit',sub, 7, 'contour');
+regressPrfSplit_symmetry(sub, 7, 'contour');
+
+end
+% 
 % for sub = 1:8
 % fprintf('%s. %d. %d ...\n','regressPrfSplit',sub, 1);
 % regressPrfSplit_control(sub, 1);
