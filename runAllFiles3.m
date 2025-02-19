9899046cd934fbeff2b5f7cc4b79243ac64a3b6c
seoheehan94 <seohee.han@mail.utoronto.ca>
2025-02-19 09:50:35 -0500

    update

--- runAllFiles3.m
+++ runAllFiles3.m
@@ -1,15 +1,15 @@
 %% prfSampleModel
 methods = {'contour', 'medialAxis', 'area'};
-
-addpath(genpath('/home/hanseohe/Documents/GitHub/3rdParty'))
-
-curmethod = 3;
-    for sub = 1:8
-        for roi = 2:4
-            fprintf('%s. %d. %d. %s ...\n','prfSampleModel',sub,roi, methods{curmethod});
-            prfSampleModel_symmetry(sub,roi, methods{curmethod});
-        end
-    end
+% 
+% addpath(genpath('/home/hanseohe/Documents/GitHub/3rdParty'))
+% 
+% curmethod = 3;
+%     for sub = 1:8
+%         for roi = 2:4
+%             fprintf('%s. %d. %d. %s ...\n','prfSampleModel',sub,roi, methods{curmethod});
+%             prfSampleModel_symmetry(sub,roi, methods{curmethod});
+%         end
+%     end
 
 
     % for sub = 1:8
@@ -41,7 +41,11 @@ curmethod = 3;
 % end
 
 %% getVoxPref
-% for sub = 1:8
-%     fprintf('%s. %d. %d ...\n','getVoxPref_regress',sub, 1);
-%     getVoxPref_regress(sub,4, 1);
-% end
+for sub = 1:8
+    fprintf('%s. %d. %d ...\n','getVoxPref_symmetry',sub, 7);
+    getVoxPref_symmetry(sub,7,'medialAxis')
+end
+for sub = 1:8
+    fprintf('%s. %d. %d ...\n','getVoxPref_symmetry',sub, 7);
+    getVoxPref_symmetry(sub,7,'area')
+end
