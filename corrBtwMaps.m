b20857e46697e91d9f712fcebe054d57cb3bdb0b
seoheehan94 <seohee.han@mail.utoronto.ca>
2025-02-27 16:02:06 -0500

    update

--- corrBtwMaps.m
+++ corrBtwMaps.m
@@ -6,13 +6,16 @@ parfolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli/parfilter/';%to s
 parList = dir(parfolder);
 parList = parList(~ismember({parList(:).name},{'.','..'}));
 
-mirfolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli/parfilter/';%to save model outputs
+mirfolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli/mirfilter/';%to save model outputs
 mirList = dir(mirfolder);
 mirList = mirList(~ismember({mirList(:).name},{'.','..'}));
 
 
 savefolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/Symmetry/corrBtwMaps/';%to save model outputs
-
+filePath = fullfile(savefolder, 'results_corrBtwMaps.mat');
+if isfile(filePath)
+    load(filePath, 'allResultsTable');
+end
 %%
 
 iimg=0;
@@ -21,18 +24,21 @@ for imgNum=1:length(parList)
     parname = parList(imgNum).name;
     mirname = mirList(imgNum).name;
 
-    filename = ['maImg' num2str(imgNum) '.mat'];
-    if ~isfile(fullfile(savefolder, filename))%if file exists already no need to remake it
 
         parModel = load(fullfile(parfolder, parname), 'model');
         mirModel = load(fullfile(mirfolder, mirname), 'model');
 
         [R,P] = corrcoef(parModel.model.contour,mirModel.model.contour);
 
+        resultsTable = table(imgNum, R(1,2), P(1,2), ...
+            'VariableNames', {'imgnum', 'R', 'P'});
 
-        
-        save(fullfile(savefolder, filename),...
-            'numFeatures','bandwidth','dims','model','numLevels');
-    end
+        if exist('allResultsTable', 'var')
+            allResultsTable = [allResultsTable; resultsTable];
+        else
+            allResultsTable = resultsTable;
+        end
+    save(filePath, 'allResultsTable');
 end
 
+% writetable(allResultsTable, 'results_corrBtwMaps.csv');
