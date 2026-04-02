ba4f3b86d25a7b50bea551d50dfd07181f2cff2f
seoheehan94 <seohee.han@mail.utoronto.ca>
2026-04-02 14:28:20 -0400

    update

--- corrBtwMaps.m
+++ corrBtwMaps.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 
-
+% --- 1. Define Directories ---
 parfolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli/parfilter/';%to save model outputs
 parList = dir(parfolder);
 parList = parList(~ismember({parList(:).name},{'.','..'}));
@@ -10,53 +10,68 @@ mirfolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli/mirfilter/';%to s
 mirList = dir(mirfolder);
 mirList = mirList(~ismember({mirList(:).name},{'.','..'}));
 
+tapfolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/stimuli/mirfilter/';%to save model outputs
+tapList = dir(tapfolder);
+tapList = tapList(~ismember({tapList(:).name},{'.','..'}));
 
 savefolder = '/bwlab/Users/SeoheeHan/NSDData/rothzn/nsd/Symmetry/corrBtwMaps/';%to save model outputs
 filePath = fullfile(savefolder, 'results_corrBtwMaps_nonzeros.mat');
 if isfile(filePath)
     load(filePath, 'allResultsTable');
 end
-%%
+%% --- 2. Process Correlations ---
 
 iimg=0;
 for imgNum=1:length(parList)
     iimg = iimg+1
     parname = parList(imgNum).name;
     mirname = mirList(imgNum).name;
+    tapname = tapList(imgNum).name;
 
 
     parModel = load(fullfile(parfolder, parname), 'model');
     mirModel = load(fullfile(mirfolder, mirname), 'model');
+    tapModel = load(fullfile(tapfolder, tapname), 'model');
 
     curpar=parModel.model.contour;
     curmir=mirModel.model.contour;
+    curtap=tapModel.model.contour;
 
-        if length(nonzeros(curpar)) == length(nonzeros(curmir))
-            curpar_nonzeros=nonzeros(curpar);
-            curmir_nonzeros=nonzeros(curmir);
-            
-        else
-            mask = (curpar == 0 & curmir ~= 0) | (curmir == 0 & curpar ~= 0);
-            % Replace all 0s in both A and B with NaN
-            curpar(curpar == 0) = NaN;
-            curmir(curmir == 0) = NaN;
-            % Restore 0s where one was originally 0 but the other was not
-            curpar(mask) = 0;
-            curmir(mask) = 0;
-            curpar_nonzeros=curpar(~isnan(curpar));
-            curmir_nonzeros=curmir(~isnan(curmir));
-        end
+    % --- Streamlined Masking Logic ---
+    % Find indices where AT LEAST ONE of the maps is non-zero.
+    % This excludes background (mutual zeros) but keeps a 0 if one map 
+    % has a signal and the others do not.
+    valid_idx = (curpar ~= 0) | (curmir ~= 0) | (curtap ~= 0);
+    
+    curpar_valid = curpar(valid_idx);
+    curmir_valid = curmir(valid_idx);
+    curtap_valid = curtap(valid_idx);
 
-        [R,P] = corrcoef(curpar_nonzeros,curmir_nonzeros);
-        resultsTable = table(imgNum, R(1,2), P(1,2), ...
-            'VariableNames', {'imgnum', 'R', 'P'});
 
-        if exist('allResultsTable', 'var')
-            allResultsTable = [allResultsTable; resultsTable];
-        else
-            allResultsTable = resultsTable;
-        end
+    % --- Calculate 3 Pairwise Correlations ---
+    % 1. Parallelism vs Mirror
+    [R_pm, P_pm] = corrcoef(curpar_valid, curmir_valid);
+    
+    % 2. Parallelism vs Taper
+    [R_pt, P_pt] = corrcoef(curpar_valid, curtap_valid);
+    
+    % 3. Mirror vs Taper
+    [R_mt, P_mt] = corrcoef(curmir_valid, curtap_valid);
+
+    % --- Store Results ---
+    % Extracting the off-diagonal elements (1,2) from the matrices
+    resultsTable = table(imgNum, R_pm(1,2), P_pm(1,2), R_pt(1,2), P_pt(1,2), R_mt(1,2), P_mt(1,2), ...
+        'VariableNames', {'imgnum', 'R_par_mir', 'P_par_mir', 'R_par_tap', 'P_par_tap', 'R_mir_tap', 'P_mir_tap'});
+    
+    if exist('allResultsTable', 'var')
+        allResultsTable = [allResultsTable; resultsTable];
+    else
+        allResultsTable = resultsTable;
+    end
+    
     save(filePath, 'allResultsTable');
+
+  
 end
 
-% writetable(allResultsTable, 'results_corrBtwMaps.csv');
+writetable(allResultsTable, 'results_corrBtwMaps.csv');
